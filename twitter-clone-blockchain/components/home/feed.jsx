fc3d00e6a1f3b2bb044eefa75fc04d537a069eda
hasan <hasanelmi678@gmail.com>
2022-07-29 18:10:05 +0100

    added posts

--- twitter-clone-blockchain/components/home/feed.jsx
+++ twitter-clone-blockchain/components/home/feed.jsx
@@ -5,6 +5,7 @@ import { BsStars } from 'react-icons/bs';
 
 // Components
 import TweetBox from './tweetBox';
+import Post from '../post';
 
 // Styles
 const styles = {
@@ -12,6 +13,35 @@ const styles = {
     header: `sticky top-0 bg-[#15202b] z-10 p-4 flex justify-between items-center`,
     headerTitle: `text-xl font-bold`
 };
+
+
+// Tweets data
+const tweets = [
+    {
+        displayName: 'Hascoin',
+        username: '0x78231878cjkbadj82uwnjsa',
+        avatar: 'https://media.pitchfork.com/photos/5cd1b36b93a536266f1ed48f/1:1/w_800,h_800,c_limit/LightInTheAttic_PacificBreeze.jpg',
+        text: 'gm',
+        isProfileImageNft: true,
+        timestamp: '2022-07-28T12:00:00.000Z'
+    },
+    {
+        displayName: 'Hascoin',
+        username: '0x78231878cjkbadj82uwnjsa',
+        avatar: 'https://media.pitchfork.com/photos/5cd1b36b93a536266f1ed48f/1:1/w_800,h_800,c_limit/LightInTheAttic_PacificBreeze.jpg',
+        text: 'gm',
+        isProfileImageNft: false,
+        timestamp: '2022-05-28T12:00:00.000Z'
+    },
+    {
+        displayName: 'Hascoin',
+        username: '0x78231878cjkbadj82uwnjsa',
+        avatar: 'https://media.pitchfork.com/photos/5cd1b36b93a536266f1ed48f/1:1/w_800,h_800,c_limit/LightInTheAttic_PacificBreeze.jpg',
+        text: 'gm',
+        isProfileImageNft: false,
+        timestamp: '2022-07-21T12:00:00.000Z'
+    },
+]
 
 const Feed = () => {
     return (
@@ -21,6 +51,17 @@ const Feed = () => {
                 <BsStars />
             </div>
             <TweetBox />
+            {tweets.map((tweet, index) => (
+                <Post 
+                key={index} 
+                displayName={tweet.displayName}
+                username={`${tweet.username.slice(0,4)}...${tweet.username.slice(-4)}`}
+                avatar={tweet.avatar}
+                text={tweet.text}
+                isProfileImageNft={tweet.isProfileImageNft} 
+                timestamp={tweet.timestamp}
+                />
+            ))}
         </div>
     )
 };
