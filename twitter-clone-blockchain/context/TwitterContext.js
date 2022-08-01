b8f87bef13c3fd31aff041e2a43100c21b8a0050
hasan <hasanelmi678@gmail.com>
2022-08-01 22:03:39 +0100

    setup sanity database

--- twitter-clone-blockchain/context/TwitterContext.js
+++ twitter-clone-blockchain/context/TwitterContext.js
@@ -1,5 +1,7 @@
 import { createContext, useEffect, useState } from 'react';
 import { useRouter } from 'next/router';
+import { client } from '../lib/client';
+
 
 export const TwitterContext = createContext();
 
@@ -16,7 +18,7 @@ export const TwitterProvider = ({ children }) => {
 
     // Checks if there is an active wallet connection.
     const checkIfWalletIsConnected = async () => {
-        if(!window.ethereum) {
+        if (!window.ethereum) {
             setAppStatus("noMetaMask");
             return;
         }
@@ -30,6 +32,7 @@ export const TwitterProvider = ({ children }) => {
                 // Connected
                 setAppStatus('connected');
                 setCurrentAccount(addressArray[0]);
+                createUserAccount(addressArray[0]);
             } else {
                 // Not connected
                 setAppStatus('notConnected');
@@ -44,7 +47,7 @@ export const TwitterProvider = ({ children }) => {
 
     // Initiates MetaMask wallet connection
     const connectToWallet = async () => {
-        if(!window.ethereum) {
+        if (!window.ethereum) {
             setAppStatus("noMetaMask");
             return;
         };
@@ -60,7 +63,7 @@ export const TwitterProvider = ({ children }) => {
                 // Connected
                 setAppStatus('connected');
                 setCurrentAccount(addressArray[0]);
-
+                createUserAccount(addressArray[0]);
             } else {
                 // Not connected
                 setAppStatus('notConnected');
@@ -72,9 +75,39 @@ export const TwitterProvider = ({ children }) => {
         }
     };
 
+    /**
+     * Creates an account in Sanity DB if the user does not already have one
+     * @param {String} userWalletAddress wallet address of the user currently logged in
+     */
+    const createUserAccount = async (userWalletAddress = currentAccount) => {
+        if (!window.ethereum) {
+            setAppStatus("noMetaMask");
+            return;
+        };
+
+        try {
+            const userDoc = {
+                _type: "users",
+                _id: userWalletAddress,
+                name: "Unnamed",
+                isProfileImageNft: false,
+                profileImage: 'https://merriam-webster.com/assets/mw/images/article/art-wap-article-main/egg-3442-e1f6463624338504cd021bf23aef8441@1x.jpg',
+                walletAddress: userWalletAddress
+            };
+
+            await client.createIfNotExists(userDoc);
+            setAppStatus("connected");
+
+        } catch (error) {
+            console.log(error);
+            setAppStatus('error');
+            router.push("/");
+        }
+    };
+
     return (
         <TwitterContext.Provider value={{ appStatus, currentAccount, connectToWallet }}>
-            { children }
+            {children}
         </TwitterContext.Provider>
     )
 }
